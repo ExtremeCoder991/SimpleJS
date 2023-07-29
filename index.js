d7e80c9e0123426814ade567945d8eac3aa99917
ExtremeCoder991 <75404903+ExtremeCoder991@users.noreply.github.com>
2023-07-29 13:05:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013 dissimulate at codepen
+Copyright (c) 2023 Noob Sniper at codepen
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
